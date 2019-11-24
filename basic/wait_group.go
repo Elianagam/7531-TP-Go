8649e67c61d87053f0877e5a14650dc256dd34af
Elianagam <elianagam2@gmail.com>
2019-11-24 15:09:14 -0300

    agrego ejemplo goroutines y wait_group

--- basic/wait_group.go
+++ basic/wait_group.go
@@ -1,19 +1,31 @@
 package main
 
-import(
-	"fmt"
-	"sync"
+import (
+    "fmt"
+    "sync"
 )
 
-func sayHello(wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Hello!")
+func f(from string, wg *sync.WaitGroup) {
+    defer wg.Done()
+    for i := 0; i < 3; i++ {
+        fmt.Println(from, ":", i)
+    }
 }
 
-func main() {
-	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go sayHello(&wg)
-	wg.Wait()
+func main() {
+    var wg sync.WaitGroup
+    wg.Add(3)
+
+    go f("goroutine1", &wg)  
+
+    // funcion anonima 
+    go func(msg string) {
+        defer wg.Done()
+        fmt.Println(msg)
+    }("going")
+
+    go f("goroutine2", &wg) 
+
+    wg.Wait()
 }
